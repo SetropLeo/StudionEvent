6694667726ba67faf8c5a351919b5f293f180ca1
LeonardoPortes <leol_portes@hotmail.com>
2021-02-22 00:50:51 -0300

    feat(finished slider carousel)

---
+++ src/script.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+  const slickOptions = {
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: false,
+    prevArrow:
+      '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
+    nextArrow:
+      '<button type="button" class="slick-next slider__next-arrow">Next</button>;',
+  };
+  $(".slider").slick(slickOptions);
+
+  $(".footer__form-button").on("click", () => {
+    const email = $("#email").val();
+    Email.send({
+      Host: "smtp.yourisp.com",
+      Username: "username",
+      Password: "password",
+      To: "leol_portes@hotmail.com",
+      From: email,
+      Subject: "Please add me to your newsletter",
+      Body:
+        'Hello! Please add me to your newsletter. That ${email} is my email.',
+    }).then((message) => alert(message));
+  });
+});
